90a186a8265ba0dce7ad39c8b6b41032652a9d45
Adetounl <bensonadetouna@gmail.com>
2022-07-02 12:47:51 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,97 +1,27 @@
-#include "main.h"
-/**
- * _strlen - string lenght
- * @str: pointer to character to be measured
- * Return: the string lenght
- */
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (*(str + len) != '\0')
-		len++;
-	return (len);
-}
-/**
- * _print - print array
- * @prod: pointer to array to be printed
- * @len1: lenght of array 1
- * @len2: lenght of array 2
- * Return: the array in the stdo
- */
-int _print(int *prod, int len1, int len2)
-{
-	int i = 0;
-/* only print one zero if are many or empty */
-for (i = 0; prod[i] == 0 || !prod[i]; i++)
-;
-/* print all starting after the last left zero */
-if (i >= (len1 + len2))
-{
-_putchar(prod[len1 + len2 - 1] + '0');
-}
-else
-for (; i <= (len1 + len2 - 1); i++)
-{
-	_putchar(prod[i] + '0');
-}
-	_putchar('\n');
-	return (0);
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _perrear - prints error message and exits with status 98
- * Return: a lot of flow yeah
- */
-void _perrear(void)
-{
-	write(1, "Error\n", 6);
-	exit(98);
-}
-/**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: argument pointer
- * Return: int
+ * main - program to multiply two positive numbers.
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
-	int mul = 0, i = 0, j = 0, k = 0, len1 = 0, len2 = 0, *prod;
-/* control only two arguments passed */
+	int mul, num1, num2;
+
 	if (argc != 3)
-	_perrear();
-/* check if the characters are all numbers */
-	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			_perrear();
-		}
+		printf("Error\n");
+		exit(98);
 	}
-/* obtain lenght of the two strings */
-len1 = _strlen(argv[1]);
-len2 = _strlen(argv[2]);
-/* allocate memory for product */
-prod = malloc(sizeof(int) * (len1 + len2));
-if (prod == NULL)
-{
-	free(prod), _perrear();
-}
-for (i = 0; i < (len1 + len2); i++)
-{
-	prod[i] = 0;
-}
-	for (j = len2 - 1; j >= 0; j--)
+	else
 	{
-		for (k = len1 - 1; k >= 0; k--)
-		{
-			mul = (argv[2][j] - '0') * (argv[1][k] - '0') + prod[j + k + 1];
-			prod[j + k + 1] = mul % 10;
-			prod[j + k] += mul / 10;
-		}
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+		return (0);
 	}
-_print(prod, len1, len2);
-free(prod);
-exit(EXIT_SUCCESS);
 }
